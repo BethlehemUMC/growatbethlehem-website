6d12b3c0e253ed9877571a360541e3dcc2b42ac8
BethlehemUMC <growatbethlehem@gmail.com>
2026-08-17 14:50:38 -0500

    Add passthrough copy for admin/config.yml

--- .eleventy.js
+++ .eleventy.js
@@ -1,1 +1,1 @@
-module.exports = function(eleventyConfig) { eleventyConfig.addPassthroughCopy("images"); eleventyConfig.addPassthroughCopy("style.css"); return { dir: { input: ".", output: "_site" } }; };
+module.exports = function(eleventyConfig) { eleventyConfig.addPassthroughCopy("images"); eleventyConfig.addPassthroughCopy("style.css"); eleventyConfig.addPassthroughCopy("admin/config.yml"); return { dir: { input: ".", output: "_site" } }; };
