e9c351a93f80b45eaebc1144874171b6003e0911
BethlehemUMC <growatbethlehem@gmail.com>
2026-08-17 11:36:15 -0500

    Add Eleventy configuration for passthrough copies

---
+++ .eleventy.js
@@ -0,0 +1,1 @@
+module.exports = function(eleventyConfig) { eleventyConfig.addPassthroughCopy("images"); eleventyConfig.addPassthroughCopy("style.css"); return { dir: { input: ".", output: "_site" } }; };
